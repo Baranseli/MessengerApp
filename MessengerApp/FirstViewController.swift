f12809df44fc88281d40dffcf5a7a6d3a4aa7573
Baranseli <baranselias@hotmail.com>
2019-05-20 00:29:08 +0300

    Initial Commit

---
+++ MessengerApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MessengerApp
+//
+//  Created by Kafkas Baranseli on 20/05/2019.
+//  Copyright © 2019 Baranseli. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MessengerApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MessengerApp
+//
+//  Created by Kafkas Baranseli on 20/05/2019.
+//  Copyright © 2019 Baranseli. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
